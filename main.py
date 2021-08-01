3989fa509e0b9af513df81b48a4e20eb2c896769
gabrielsantos2000 <gabrielsantos2000@gmail.com>
2021-08-01 18:22:58 -0300

    removing methos from main file

--- main.py
+++ main.py
@@ -5,12 +5,3 @@ from challenges import Challenges
 # Ex: challenges.day_three_intro_to_condition_statements()
 
 challenges = Challenges()
-
-#challenges.day_four_class_vs_instance()
-
-n = int(input().strip())
-
-if 2 <= n <= 20:
-    for i in range(1, 11):
-        result = n * i
-        print(n, "x", i, "=", result)
