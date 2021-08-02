bcb060dc3a76c349c9785587cbbff4a9e8070a2f
gabrielsantos2000 <oldsgabriel@gmail.com>
2021-08-02 18:13:21 -0300

    adding challenge 7 - day 6

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ from challenges import Challenges
 # Ex: challenges.day_three_intro_to_condition_statements()
 
 challenges = Challenges()
+
+t = int(input().strip())
+
+for i in range(0, t):
+
+    s = list(input().strip())
+    str_par = ''
+    str_impar = ''
+
+    for j in range(0, len(s)):
+        if j % 2 == 0:
+            str_par += s[j]
+        else:
+            str_impar += s[j]
+    else:
+        print(str_par, str_impar)
