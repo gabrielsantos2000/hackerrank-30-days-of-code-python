ee0a4ea1d7e0251073aef8f9a7b90b03c5a8e51b
Gabriel Oliveira <30851944+gabrielsantos2000@users.noreply.github.com>
2021-08-03 20:36:07 -0300

    Adding the challenge description

--- day_7_arrays.py
+++ day_7_arrays.py
@@ -1,3 +1,11 @@
+# Task
+# Given an array, A, of N integers, print A's elements in reverse order 
+# as a single line of space-separated numbers.
+
+# Difficulty: Easy
+# Score: 30
+# Challenge link: https://www.hackerrank.com/challenges/30-arrays/problem
+
 n = int(input().strip())
 
 arr = list(map(int, input().rstrip().split()))
